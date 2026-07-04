6c5e79626e8f0a4e95132b051f6b485b67b55372
Cida Luna <cidalunna@hotmail.com>
2026-07-04 18:35:31 -0300

    feat: update app component

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,10 +2,25 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideTransloco } from '@jsverse/transloco';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { languageHeaderInterceptor } from './core/interceptors/language-header.interceptor';
+import { TranslocoHttpLoader } from './core/i18n/transloco-loader';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
-  ]
+    provideRouter(routes),
+    provideHttpClient(withInterceptors([languageHeaderInterceptor])),
+    provideTransloco({
+      config: {
+        availableLangs: ['pt-BR', 'en-US'],
+        defaultLang: 'pt-BR',
+        fallbackLang: 'pt-BR',
+        reRenderOnLangChange: true,
+        prodMode: false,
+      },
+      loader: TranslocoHttpLoader,
+    }),
+  ],
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,4 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/components/home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [{ path: '', component: Home }];
