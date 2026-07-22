dfc68dbc71e188639cab5bcebd751a2d406682e2
Cida Luna <cidalunna@hotmail.com>
2026-07-22 00:49:27 -0300

    feat: update app config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,25 +2,11 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideTransloco, provideTranslocoMissingHandler, TranslocoMissingHandler, TranslocoMissingHandlerData } from '@jsverse/transloco';
+import { provideTransloco } from '@jsverse/transloco';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { languageHeaderInterceptor } from './core/interceptors/language-header.interceptor';
 import { TranslocoHttpLoader } from './core/i18n/transloco-loader';
-import { environment } from '../environments/environment'; // Importação estática padrão
-
-/**
- * CLASSE DE PROTEÇÃO CONTRA VAZAMENTO DE CÓDIGO
- * Se a chave falhar em todos os arquivos JSON, ela barra a string crua.
- */
-export class StrictMissingTranslationHandler implements TranslocoMissingHandler {
-  handle(key: string, data: TranslocoMissingHandlerData): string {
-    console.error(`🚨 CRÍTICO [i18n]: A chave "${key}" não foi localizada em nenhum dicionário.`);
-
-    // Retorna três pontos ou string vazia. O usuário NUNCA verá "home.greeting" na tela.
-    // Apenas para exemplificar, garantir com produto que essa configuração não entra. Foi só teste.
-    return '...';
-  }
-}
+import { environment } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -34,16 +20,14 @@ export const appConfig: ApplicationConfig = {
         defaultLang: 'pt-BR',
         fallbackLang: 'pt-BR',
         reRenderOnLangChange: true,
-        prodMode: true, // environment.i18n.prodMode, Consome a flag do ambiente ativo
+        prodMode: environment.i18n.prodMode, //Consome a flag do ambiente ativo
         missingHandler: {
-          logMissingKey: true,
-          useFallbackTranslation: true, // Busca no pt-BR antes de desistir
-          allowEmpty: false, // Não aceita chaves vazias ("") como tradução válida
+          logMissingKey: true,          // Exibe erro no console do navegador
+          useFallbackTranslation: true, // Busca no idioma padrão antes de acusar erro
+          allowEmpty: false,            // Não aceita chaves vazias ("") como tradução válida
         }
       },
       loader: TranslocoHttpLoader,
     }),
-    // 2. INJEÇÃO DO PROVEDOR CUSTOMIZADO (Forma tipada correta para o Angular 21). Só teste, não usar.
-    provideTranslocoMissingHandler(StrictMissingTranslationHandler)
   ],
 };
