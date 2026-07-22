a13bcf979582d81d55fd0ad8a9f38a4e15600fd0
Cida Luna <cidalunna@hotmail.com>
2026-07-21 22:37:36 -0300

    feat: improve home component data

--- src/app/home/components/home/home.ts
+++ src/app/home/components/home/home.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, inject, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, OnInit, signal, WritableSignal } from '@angular/core';
 import { TranslocoModule } from '@jsverse/transloco';
 import { HomeDataService } from './services/home-data.service';
 
@@ -14,8 +14,16 @@ import { HomeDataService } from './services/home-data.service';
 export class Home implements OnInit {
   readonly homeDataService = inject(HomeDataService);
 
+  // DECLARAÇÃO E INICIALIZAÇÃO IMEDIATA (Padrão Angular 18)
+  readonly nomeA = signal<string>('Cida');
+  readonly qtdA = signal<number>(5);
+
   ngOnInit(): void {
     // Carrega já no idioma commitado (o que veio do localStorage, ou pt-BR).
     this.homeDataService.load();
+  }
+
+  abrirModalConfirmacao() {
+    // teste
   }
 }
